281f9fcd507ad0e868b47224bd4e6ca2b633e57e
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-10-03 21:11:52 -0500

    Layer selection in UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,12 +3,14 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
-}
+    }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
-}
+    }
+
+void MainWindow::on_layerNumSB_valueChanged(int layerNum) {
+    ui->neurons2SB->setEnabled(layerNum == 2 ? true : false);
+    }
